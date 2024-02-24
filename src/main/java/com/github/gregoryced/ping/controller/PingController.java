25cd73100a95bbcc889c9e0b0ca7f3c5c3c4dbe7
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-23 22:54:05 -0300

    Feito a classe controller

---
+++ src/main/java/com/github/gregoryced/ping/controller/PingController.java
@@ -0,0 +1,22 @@
+package com.github.gregoryced.ping.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PingController {
+	
+	@GetMapping("/ping")
+	public String ping() {
+		return "Pong";
+	}
+	
+	@GetMapping("/nome")
+	public String nome() {
+		return("Gregory");
+	}
+	
+	
+}
